ba04bc571850d3d3504e44b0afc4aba39ca4b416
Marcelo <marsleite@gmail.com>
2021-10-25 13:33:11 -0300

    gerando o token e authenticando com ele

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -8,17 +8,28 @@ import axios from "axios";
   * retornar o token com as informções do usuário
 */
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
 class AuthenticateUserService {
   async execute(code: string){
    const url = `https://github.com/login/oauth/access_token`;
 
-    const response = await axios.post(url, null, { 
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, { 
       params: {
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
       code,
       },
       headers: {  "Accept": "application/json" },
+    });
+
+    const response = await axios.get(`https://api.github.com/user`, {
+      headers: {  
+        "Authorization": `Bearer ${accessTokenResponse.access_token}`,
+        "Accept": "application/json",
+      },
     });
 
     return response.data;
